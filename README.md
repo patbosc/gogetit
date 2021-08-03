a74b3ead845216344a1c09318fd97ddbffbdb92e
Patric Boscolo <patric@imenja.io>
2021-08-03 22:40:28 +0200

    added basic functionality for crawler no rewrites just a simple http server which listens to ping and search

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# gogetit
-a crawler wirtten in go because uneidel tells me all day how awesome go is :P
+# go get it
+
+a crawler wirtten in go because uneidel tells me all day long how awesome go is :P
+
+Using colly which lives 
+[on Github ](https://github.com/gocolly/colly) thanks to the [docs no one finds](http://go-colly.org/docs/) because of the [friendly crawler documentation on gopkg](https://pkg.go.dev/github.com/gocolly/colly#section-readme) 
+
+First  installing Colly like this:
+
+```
+go get -u github.com/gocolly/colly/...
+```
+
+but you also need the go modules file to be like
+
+```
+ github.com/gocolly/colly/v2 v2.1.0
+```
+
+Okay the little service is a httpListener 
+
+Supported Commands:
+- Ping 
+```
+curl 'http://127.0.0.1:7272/ping
+```
+- Search
+```
+curl 'http://127.0.0.1:7272/search?url=http://www.google.com
+```
+
+Check out this stuff as well: 
+--
+- https://github.com/el10savio/GoCrawler
+- http://go-colly.org/
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gocolly/colly"
+)
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	log.Println("Ping")
+	w.Write([]byte("ping"))
+}
+
+func main() {
+
+	// create a service to listen on port 7777
+	addr := ":7272"
+
+	http.HandleFunc("/search", getIt)
+	http.HandleFunc("/ping", ping)
+
+	log.Println("listening on", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func getIt(w http.ResponseWriter, r *http.Request) {
+	//Verify URL
+	URL := r.URL.Query().Get("url")
+	if URL == "" {
+		log.Println("missing URL argument")
+		return
+	}
+	log.Println("visiting", URL)
+
+	//Create a new colly collector
+	c := colly.NewCollector()
+
+	var httpResponse []string
+
+	//check if the website html has any links if so get it
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		if link != "" {
+			httpResponse = append(httpResponse, link)
+		}
+	})
+
+	//run clever boy, run
+	c.Visit(URL)
+
+	//now parse our response to JSON
+
+	b, err := json.Marshal(httpResponse)
+	if err != nil {
+		log.Println("failed to serialize response:", err)
+		return
+	}
+	//Add some header and write the body
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(b)
+}
